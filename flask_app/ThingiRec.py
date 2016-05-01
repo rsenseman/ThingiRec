a0f4fbed0b32b5e4b010b6eafe1101e56b9e4a0e
rsenseman <robert.senseman@gmail.com>
2016-04-30 21:15:45 -0700

    bootstrap_implemented

--- flask_app/ThingiRec.py
+++ flask_app/ThingiRec.py
@@ -45,45 +45,53 @@ def get_top_users_and_parts(user_ind, username):
         else:
             n_items += 1
 
-    users_stats = np.unique(similar_users, return_counts = True)
-    parts_stats = np.unique(similar_parts, return_counts = True)
-    # print "user stats:"
-    # print sorted(zip(*users_stats), key = lambda x: 1.0/x[1])
-    # print "part stats:"
-    # print sorted(zip(*parts_stats), key = lambda x: 1.0/x[1])
     top_similar_users = sorted(similar_users_set, key = lambda x: similar_users.count(x), reverse = True)[:5]
+
     top_similar_parts = sorted(similar_parts_set, key = lambda x: similar_parts.count(x), reverse = True)[:5]
-    return top_similar_users, top_similar_parts
+    similar_parts_df = full_df[full_df['item_id'].isin(top_similar_parts)].set_index('item_id')
+    part_names = similar_parts_df['item_name']
+    part_ids = part_names.keys()
+
+    # limit length of part names
+    max_len = 15
+
+    part_names = map(lambda x: x[:max_len+1]+'...' if len(x) > max_len else x,part_names)
+
+    return top_similar_users, part_ids, part_names
 
 @app.route('/')
 def api_root():
     return render_template('home.html')
 
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
 @app.route('/recommend', methods=['POST'])
 def recommend():
     rec_start = time.time()
     print "recommending!!!"
-    username = str(request.form['user_input'])
+    username = str(request.form['user_input']).lower()
     user_ind = np.ravel(np.argwhere(full_df['username']==username))
-    
-    # limit number of parts to be used for similarity analysis    
-    num_user_parts = min(20,len(user_ind))
-    user_ind=np.random.choice(user_ind,(num_user_parts,))
 
     # if the username is found, get recommendations
     # otherwise, reroute user to try again
     if user_ind.any():
-        users, parts = get_top_users_and_parts(user_ind, username)
+        # limit number of parts to be used for similarity analysis
+        num_user_parts = min(20,len(user_ind))
+        user_ind=np.random.choice(user_ind,(num_user_parts,))
+
+        users, part_numbers, part_names = get_top_users_and_parts(user_ind, username)
         rec_end = time.time()
         print "time to recommend: {}".format(rec_end-rec_start)
-        return render_template('recommend.html', users=users, parts=parts)
+        return render_template('recommend.html', users=users, part_numbers=part_numbers, part_names=part_names)
     else:
         print "empty dataframe"
-        return render_template('try_again.html') # render_template('home.html')
+        return render_template('try_again.html', username=username)
 
 @app.route('/contact')
 def contact():
-    return ''' '''
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     boot_start = time.time()
@@ -95,11 +103,14 @@ if __name__ == '__main__':
     SQL = "SELECT * FROM thingi_items ORDER BY item_id"
     c.execute(SQL)
     full_df = pd.DataFrame(c.fetchall(), columns = ['item_id','item_name','description','username'])
+    full_df['username']=full_df['username'].apply(lambda x: x.lower())
+    full_df['item_name']=full_df['item_name'].apply(lambda x: x.lower())
+    full_df['description']=full_df['description'].apply(lambda x: x.lower())
 
     # transform names and descriptions
     tfidfvect = TfidfVectorizer(stop_words='english', max_features = 1000)
     documents_vec = full_df['item_name'] + ' ' + full_df['description']
-    vec_X = tfidfvect.fit_transform(documents_vec)#.toarray()
+    vec_X = tfidfvect.fit_transform(documents_vec)
     del documents_vec
 
     boot_end = time.time()
@@ -107,4 +118,4 @@ if __name__ == '__main__':
     print "boot time: {}".format(boot_end-boot_start)
 
     # Start Flask app
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=False)
